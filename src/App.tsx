fa901f8803dc79158e24ed0c4f89c81c4b74809d
Hadilou1998 <hadilissilame@gmail.com>
2024-07-21 02:04:43 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import About from "./components/About";
@@ -7,22 +8,49 @@ import Favorites from "./components/FavoritesRecipes";
 import { RecipeProvider } from "./components/RecipeContext/Context";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./components/LandingPage";
+import Data from "./components/Data";
+
+interface Recipe {
+  id: number;
+  name: string;
+  cuisine: string;
+  photo: string;
+  ingredients: string;
+  preparation: string;
+}
 
 export default function App() {
+  
+  const [recipes, setRecipes] = useState<Recipe[]>(Data.map((recipe, index) => ({
+    ...recipe,
+    id: index + 1,
+    ingredients: recipe.ingredients.join(', ')
+  })));
+
+  const addRecipe = (recipe: Omit<Recipe, 'id'>) => {
+    const newRecipe: Recipe = {
+      ...recipe,
+      id: recipes.length + 1
+    };
+    setRecipes([...recipes, newRecipe]);
+  };
+
+  const deleteRecipe = (index: number) => {
+    setRecipes(recipes.filter((recipe, i) => i !== index));
+  };
+
   return (
-    <Router>
-      <div className="App">
-        <RecipeProvider>
-          <Header />
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/recipes" element={<RecipeList />} />
-            <Route path="/favorites" element={<Favorites />} />
-            <Route path="/add-recipe" element={<RecipeForm />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </RecipeProvider>
-      </div>
-    </Router>
+    <div className="App">
+      <RecipeProvider>
+        <Header />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/recipes" element={<RecipeList recipes={recipes} deleteRecipe={deleteRecipe} />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/add-recipe" element={<RecipeForm handleAddRecipe={addRecipe} />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </RecipeProvider>
+    </div>
   );
 }
