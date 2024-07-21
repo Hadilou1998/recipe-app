24bd8a0f1a97a27f33ab06ab6afdbbc2e2e18728
Hadilou1998 <hadilissilame@gmail.com>
2024-07-21 20:56:20 +0200

    Updated 4 files in 1 commits

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"; // Importation de Link pour les liens de navigation
 
-const Header = () => {
-    return (
-        <nav>
-            <ul className="menu">
-                <li className="logo">
+const Header = () => { // Définition du composant Header
+    return ( // Rendu du composant
+        <nav> {/* Navigation principale */}
+            <ul className="menu"> {/* Liste de liens de navigation */}
+                <li className="logo"> {/* Logo de l'application */}
                     <Link to="/">RECIPE APP</Link>
                 </li> 
-                <li className="item">
-                    <Link to="/">Home</Link>
+                <li className="item"> 
+                    <Link to="/">Home</Link> {/* Lien vers la page d'accueil */}
+                </li>
+                <li className="item"> 
+                    <Link to="/recipes">Recipes</Link> {/* Lien vers la page des recettes */}
                 </li>
                 <li className="item">
-                    <Link to="/recipes">Recipes</Link>
+                    <Link to="/favorites">Favorites</Link> {/* Lien vers la page des recettes favorites */}
                 </li>
                 <li className="item">
-                    <Link to="/favorites">Favorites</Link>
+                    <Link to="/add-recipe">Add Recipe</Link> {/* Lien vers la page d'ajout de recette */}
                 </li>
                 <li className="item">
-                    <Link to="/add-recipe">Add Recipe</Link>
-                </li>
-                <li className="item">
-                    <Link to="/about">About</Link>
+                    <Link to="/about">About</Link> {/* Lien vers la page " propos" */}
                 </li>
             </ul>
         </nav>
     );
 };
 
-export default Header;
+export default Header; // Exportation du composant Header comme exportation par défaut
--- src/components/Types.tsx
+++ src/components/Types.tsx
@@ -1,6 +1,6 @@
-export const UPDATE_FAV_RECIPES = "UPDATE_FAV_RECIPES";
-export const FETCH_RECIPES = "FETCH_RECIPES";
-export const FETCH_RECIPE = "FETCH_RECIPE";
-export const TOGGLE_MODAL = "TOGGLE_MODAL";
-export const TOGGLE_LOADING = "TOGGLE_LOADING";
-export const SET_ERROR = "SET_ERROR";
+export const UPDATE_FAV_RECIPES = "UPDATE_FAV_RECIPES"; // Définition des types d'actions pour les recettes favorites
+export const FETCH_RECIPES = "FETCH_RECIPES"; // Définition des types d'actions pour les recettes
+export const FETCH_RECIPE = "FETCH_RECIPE"; // Définition des types d'actions pour une recette spécifique
+export const TOGGLE_MODAL = "TOGGLE_MODAL"; // Définition des types d'actions pour le modal
+export const TOGGLE_LOADING = "TOGGLE_LOADING"; // Définition des types d'actions pour le chargement des données
+export const SET_ERROR = "SET_ERROR"; // Définition des types d'actions pour l'affichage d'erre
