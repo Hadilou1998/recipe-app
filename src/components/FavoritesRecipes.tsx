54883e96a15a88accfbe44c24980b22dec1eb4ce
Hadilou1998 <hadilissilame@gmail.com>
2024-07-21 16:15:52 +0200

    Updated FavoritesRecipes.tsx

--- src/components/FavoritesRecipes.tsx
+++ src/components/FavoritesRecipes.tsx
@@ -12,15 +12,10 @@ const Favorites = () => {
             <h1>Mes recettes favorites :</h1>
             {context.favorites.slice(0, 4).map((recipe: any, index: number) => (
                 <div key={index} className="favorite-recipe">
-                    <div className="favorite-image">
-                        <img src={recipe.photo} alt={recipe.name} />
-                    </div>
-                    <div className="favorite-content">
-                        <h2>{recipe.name}</h2>
-                        <p>{recipe.cuisine}</p>
-                        <p>{recipe.ingredients}</p>
-                        <p>{recipe.preparation}</p>
-                    </div>
+                    <RecipeList 
+                    recipes={[recipe]}
+                    deleteRecipe={() => context.deleteFavorite(index)}
+                    />
                 </div>
             ))}
         </div>
